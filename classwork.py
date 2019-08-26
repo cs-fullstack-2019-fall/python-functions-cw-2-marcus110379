d5813343c21f08affaf18edfc98179eccb85023d
marcus110379 <marcusjmadden@gmail.com>
2019-08-26 12:06:49 -0500

    classwork

---
+++ classwork.py
@@ -0,0 +1,13 @@
+## Problem 1:
+#Create a function that will ask the user for a number. Use the function to get two numbers from the user, then pass the two numbers to a function. Add, subtract, multiple, and divide the numbers.
+def ask(): # this function ask for 2 inputs puts input into num function call
+    userInput = int(input("enter a number "))
+    userInput2 = int(input("enter another number "))
+    num(userInput, userInput2)
+def num(num1, num2): # this function uses input from ask function and then prints the add, subtract, multiply , and division of  it
+    print(num1 + num2)
+    print(num1 - num2)
+    print(num1 * num2)
+    print(num1 / num2)
+
+ask()
